33b8a75159b7d2c5e92f40856b9afe3dd09f74c0
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-04 00:09:18 +0300

    add Task 001 Friends And Dog

---
+++ Task001_FriendsAndDog/Program.cs
@@ -0,0 +1,24 @@
+﻿double distance = 10000,
+        time = 0;
+byte firstFriendSpeed = 1,
+    secondFriendSpeed = 2,
+    dogSpeed = 5, 
+    friend = 2,
+    quantiti = 0,
+    meet = 10;
+while(distance > meet)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+quantiti++;
+}
+Console.WriteLine("Собака пробежала " + quantiti + " раз.");
